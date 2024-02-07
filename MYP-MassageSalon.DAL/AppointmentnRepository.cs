29dfa5d5fc52d14af4e0670544ea0f3385c0e074
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-08 00:00:29 +0300

    add pr AddAppointments

---
+++ MYP-MassageSalon.DAL/AppointmentnRepository.cs
@@ -0,0 +1,25 @@
+﻿using MYP_MassageSalon.DAL.IRepositories;
+using System;
+using MYP_MassageSalon.DAL.Dtos;
+using MYP_MassageSalon.DAL.StoredProcedures;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace MYP_MassageSalon.DAL
+{
+    public class AppointmentnRepository: IAppointmentnRepository
+    {
+        public void AddAppointment(AppointmentDTO appointment) //ГЛОБАЛЬНО добавляем заявку (заводим по id клиента)
+        {
+            using (IDbConnection connection = new SqlConnection(Options.ConStr))
+            {
+                connection.Query(AppointmentStoredProcedures.AddAppointment,
+                    new { appointment.ClientId},
+                    commandType: CommandType.StoredProcedure); //тип подключения ??
+            }
+        }
+
+    }
+}
