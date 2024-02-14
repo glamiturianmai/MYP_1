cfd0892345d5b3c2fe9532d2f2c0d4ee05e36e74
kihoii <kihikolee@gmail.com>
2024-02-14 22:56:35 +0300

    delete consoleapp1

---
+++ MYP_MassageSalon.TG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MYP_MassageSalon.TG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
