f44b67b0ea19ba98fe65e6b0b398af463e78e0a2
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-21 17:42:54 +0300

    some test

---
+++ please-test/Program.cs
@@ -0,0 +1,7 @@
+﻿using MYP_MassageSalon.DAL.Dtos;
+using MYP_MassageSalon.DAL;
+
+WorkersRepository workRep = new WorkersRepository();
+var a = workRep.GetWorkersByServiceId(2);
+
+Console.ReadLine();
