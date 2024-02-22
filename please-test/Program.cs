9b9c7c070c085019a6c3954671bdd2b41c808e56
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-22 19:27:43 +0300

    test

--- please-test/Program.cs
+++ please-test/Program.cs
@@ -1,7 +1,18 @@
 ﻿using MYP_MassageSalon.DAL.Dtos;
 using MYP_MassageSalon.DAL;
+using MYP_MassageSalon.BLL;
 
-ServiceRepository workRep = new ServiceRepository();
-List<ServicesDTO> a = workRep.GetAllServicesName();
+//WorkersRepository workRep = new WorkersRepository();
+//List<WorkersDTO> a = workRep.GetWorkersByServiceId(2);
+
+WorkerClient workrep = new WorkerClient();
+var b = workrep.GetWorkersByServiceIdMap(2);
+
+
+ClientRepository workRep = new ClientRepository();
+List<ClientsDTO> a = workRep.GetClientsAppointments(1);
+
+AppointmentClient appClient = new AppointmentClient();
+var d = appClient.GetClientsAppointmentsMap(1);
 
 Console.ReadLine();
