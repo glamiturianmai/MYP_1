ba09077f3079701a2c2169f3ea0fec3f34bef3be
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-23 01:29:56 +0300

    try to test

--- please-test/Program.cs
+++ please-test/Program.cs
@@ -1,18 +1,21 @@
 ﻿using MYP_MassageSalon.DAL.Dtos;
 using MYP_MassageSalon.DAL;
 using MYP_MassageSalon.BLL;
+using MYP_MassageSalon.BLL.Models.InputModels;
 
 //WorkersRepository workRep = new WorkersRepository();
 //List<WorkersDTO> a = workRep.GetWorkersByServiceId(2);
 
-WorkerClient workrep = new WorkerClient();
-var b = workrep.GetWorkersByServiceIdMap(2);
+//WorkerClient workrep = new WorkerClient();
+//var b = workrep.GetWorkersByServiceIdMap(2);
 
 
-ClientRepository workRep = new ClientRepository();
-List<ClientsDTO> a = workRep.GetClientsAppointments(1);
+//ClientRepository workRep = new ClientRepository();
+//List<ClientsDTO> a = workRep.GetClientsAppointments(1);
 
+DeleteAppIntputModel q = new DeleteAppIntputModel();
+q.Id = 1;
 AppointmentClient appClient = new AppointmentClient();
-var d = appClient.GetClientsAppointmentsMap(1);
+appClient.DeleteAppointmentMap(q);
 
 Console.ReadLine();
