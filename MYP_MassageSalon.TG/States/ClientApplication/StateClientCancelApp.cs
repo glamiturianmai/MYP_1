7ee7401fc6c517c90a89d3fd2d5b0e0374b5cb20
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-23 10:33:55 +0300

    StateClientCancelApp

---
+++ MYP_MassageSalon.TG/States/ClientApplication/StateClientCancelApp.cs
@@ -0,0 +1,61 @@
+﻿using MYP_MassageSalon.BLL.Models.InputModels;
+using MYP_MassageSalon.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace MYP_MassageSalon.TG.States.ClientApplication
+{
+    public class StateClientCancelApp : AbstractState
+    {
+        private int _appId;
+        private AppointmentClient _appClient;
+        private DeleteAppIntputModel _appTG;
+
+
+        public StateClientCancelApp(int appId)
+        {
+            _appId = appId;
+            _appClient = new AppointmentClient();
+            _appTG = new DeleteAppIntputModel();
+            _appTG.Id = appId;
+        }
+
+
+
+        public override AbstractState ReceiveMessage(Update update)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                return new StartState();
+            }
+            return this;
+        }
+
+        public override void SendMessage(long chatId)
+        {
+
+            _appClient.DeleteAppointmentMap(_appTG); // не работает 
+
+
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(
+                   new InlineKeyboardButton[][]
+                   {
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Спасибо!") {CallbackData="SeeApps"}
+
+                        }
+                   }
+                   );
+
+            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"Мы отменили вашу заявку!", replyMarkup: markup);
+        }
+    }
+}
---
+++ MYP_MassageSalon.TG/States/ClientApplication/StateClientCancelAskApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace MYP_MassageSalon.TG.States.ClientApplication
+{
+    public class StateClientDeleteAskApp : AbstractState
+    {
+        private int _appId;
+        public StateClientDeleteAskApp(int id)
+        {
+            _appId = id;
+
+        }
+
+
+
+        public override AbstractState ReceiveMessage(Update update)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                
+                int appId = _appId;
+                var message = update.CallbackQuery.Data;
+
+                if (message == "ok")
+                {
+                    return new StateClientCancelApp(appId);
+                }
+                else if (message == "back")
+                {
+                    return new StateClientDoWithApp(appId);
+                }
+               
+            }
+            return this;
+        }
+
+        public override void SendMessage(long chatId)
+        {
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(
+                    new InlineKeyboardButton[][]
+                    {
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Да, хочу отменить") {CallbackData="ok"}
+
+                        },
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("назад!") {CallbackData="back"}
+                        }
+                    }
+                    );
+            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"А вы уверены?", replyMarkup: markup);
+        }
+    }
+}
