3913115f709204ab38b7eddeb5542e0eb5d50ed1
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-23 14:11:38 +0300

    test

--- please-test/Program.cs
+++ please-test/Program.cs
@@ -2,6 +2,7 @@
 using MYP_MassageSalon.DAL;
 using MYP_MassageSalon.BLL;
 using MYP_MassageSalon.BLL.Models.InputModels;
+using MYP_MassageSalon.BLL.Models.OutputModels;
 
 //WorkersRepository workRep = new WorkersRepository();
 //List<WorkersDTO> a = workRep.GetWorkersByServiceId(2);
@@ -13,9 +14,21 @@ using MYP_MassageSalon.BLL.Models.InputModels;
 //ClientRepository workRep = new ClientRepository();
 //List<ClientsDTO> a = workRep.GetClientsAppointments(1);
 
-DeleteAppIntputModel q = new DeleteAppIntputModel();
-q.Id = 1;
-AppointmentClient appClient = new AppointmentClient();
-appClient.DeleteAppointmentMap(q);
+//DeleteAppIntputModel q = new DeleteAppIntputModel();
+//q.Id = 1;
+//AppointmentClient appClient = new AppointmentClient();
+//appClient.DeleteAppointmentMap(q);
+
+ClientsDTO qq = new ClientsDTO();
+
+ClientRepository apClient = new ClientRepository();
+var a = apClient.GetClientsAppointments(2);
+
+
+//AppointmentsOutputModel q = new AppointmentsOutputModel();
+//q.Id = 2;
+//AppointmentClient appClient = new AppointmentClient();
+//var a = appClient.GetClientsAppointmentsMap(2);
+
 
 Console.ReadLine();
