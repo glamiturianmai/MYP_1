3f30156ac361dd6484dc4cd703309f7558eadba5
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-24 14:17:53 +0300

    new test

---
+++ not gitignor nastya please/Program.cs
@@ -0,0 +1,22 @@
+﻿using MYP_MassageSalon.BLL;
+using MYP_MassageSalon.BLL.Models.InputModels;
+using MYP_MassageSalon.BLL.Models.OutputModels;
+using MYP_MassageSalon.DAL;
+using MYP_MassageSalon.DAL.Dtos;
+
+QualificationDTO workersDTO = new QualificationDTO();
+WorkersRepository r = new WorkersRepository();
+
+var b = r.GetQualifWorker();
+
+
+
+
+
+WorkerClient q = new WorkerClient();
+QualificationsOutputModel m =  new QualificationsOutputModel();
+
+var a = q.GetQualifWorker();
+
+
+Console.ReadLine();
