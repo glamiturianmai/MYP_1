05dfbcdd8ad5f5660ce4194da41d497a5154f80d
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-24 18:53:59 +0300

    adnin services add - dal + test

--- not gitignor nastya please/Program.cs
+++ not gitignor nastya please/Program.cs
@@ -4,20 +4,25 @@ using MYP_MassageSalon.BLL.Models.OutputModels;
 using MYP_MassageSalon.DAL;
 using MYP_MassageSalon.DAL.Dtos;
 
-//QualificationDTO workersDTO = new QualificationDTO();
-//WorkersRepository r = new WorkersRepository();
+//ServicesDTO dto = new ServicesDTO();
+//ServiceRepository r = new ServiceRepository();
+//dto.Name = "скульптурирующий массаж";
+//dto.Price = 4200;
+//dto.Time = 75;
+//r.SetService(dto);
 
-//var b = r.GetQualifWorker();
 
 
 
 
-
-WorkerClient q = new WorkerClient();
-WorkersAddInputModel m =  new WorkersAddInputModel();
-m.Name = "aaa";
-m.QualificationId = 1;
-q.AddNewWorkerMap(m);
-
+ServiceClient q = new ServiceClient();
+ServiceIntputModel dto = new ServiceIntputModel();
+//m.Name = "aaa";
+//m.QualificationId = 1;
+//q.AddNewWorkerMap(m);
+dto.Name = "лимфодренажный массаж";
+dto.Price = 3800;
+dto.Time = 60;
+q.SetServiceMap(dto);
 
 Console.ReadLine();
