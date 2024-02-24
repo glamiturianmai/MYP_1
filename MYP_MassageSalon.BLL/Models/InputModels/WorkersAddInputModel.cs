8305e22843bb225f7a8787a2eb391e20592ffe0e
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-24 17:24:14 +0300

    admin add master - bll

---
+++ MYP_MassageSalon.BLL/Models/InputModels/WorkersAddInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYP_MassageSalon.BLL.Models.InputModels
+{
+    public class WorkersAddInputModel
+
+    {
+        public string Name { get; set; }
+        public int QualificationId { get; set; }
+
+    }
+}
