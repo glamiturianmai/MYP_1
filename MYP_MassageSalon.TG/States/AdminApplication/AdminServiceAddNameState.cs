36e1f73641d1154bc7784092d45d0f531614d68a
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-24 18:53:07 +0300

    adnin services add - tg

---
+++ MYP_MassageSalon.TG/States/AdminApplication/AdminServiceAddNameState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace MYP_MassageSalon.TG.States.AdminApplication
+{
+    public class AdminServiceAddNameState : AbstractState //вводим имя мастера
+    {
+
+
+
+        public override AbstractState ReceiveMessage(Update update)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                var name = update.Message.Text.ToString();
+                return new AdminServiceAddCostState(name);
+            }
+            return this;
+        }
+
+        public override void SendMessage(long chatId)
+        {
+
+            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"Введите название услуги:");
+        }
+
+    }
+
+}
--- MYP_MassageSalon.TG/States/AdminApplication/AdminServiceDoState.cs
+++ MYP_MassageSalon.TG/States/AdminApplication/AdminServiceDoState.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MYP_MassageSalon.TG.States;
-using MYP_MassageSalon.TG.States.ClientApplication;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
 using Telegram.Bot;
 
 namespace MYP_MassageSalon.TG.States.AdminApplication
 {
-    public class AdminStartState : AbstractState
+    public class AdminServiceDoState: AbstractState
     {
         public override AbstractState ReceiveMessage(Update update)
         {
@@ -20,28 +18,29 @@ namespace MYP_MassageSalon.TG.States.AdminApplication
             {
                 var message = update.CallbackQuery.Data;
 
-                if (message == "workers")
-                {
-
-                    return new AdminWorkerDoState();
-                }
-                else if (message == "services")
-                {
-                    return new AdminServiceDoState();
-                }
-                else if (message == "appointments")
+                if (message == "see")
                 {
                     return this;
+
+                    //return new AdminWorkerSeeState();
                 }
-                else if (message == "sheduleinterval")
+                else if (message == "add")
                 {
-                    return new AdminSetSheduleState();
+                    return new AdminServiceAddNameState();
+                }
+                else if (message == "back")
+                {
+                    return new AdminStartState();
+                }
+                else if (message == "home")
+                {
+                    return new StartState();
                 }
                 else return this;
 
             }
             else return this;
-            
+
         }
 
         public override void SendMessage(long chatId)
@@ -54,25 +53,24 @@ namespace MYP_MassageSalon.TG.States.AdminApplication
                 {
                         new InlineKeyboardButton[]
                         {
-                            new InlineKeyboardButton("Мастера") {CallbackData="workers"}
+                            new InlineKeyboardButton("Наши услуги") {CallbackData="see"}
 
                         },
                         new InlineKeyboardButton[]
                         {
-                            new InlineKeyboardButton("Услуги") {CallbackData="services"}
+                            new InlineKeyboardButton("Добавить") {CallbackData="add"}
                         },
                         new InlineKeyboardButton[]
                         {
-                            new InlineKeyboardButton("Записи") {CallbackData="appointments"}
+                            new InlineKeyboardButton("назад!") {CallbackData="back"}
                         },
                         new InlineKeyboardButton[]
                         {
-                            new InlineKeyboardButton("ДЛЯ НАСТИ") {CallbackData="sheduleinterval"}
+                            new InlineKeyboardButton("домой") {CallbackData="home"}
                         }
                 }
                 );
-            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"Здравствуйте, администратор!", replyMarkup: markup);
+            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"Что будем делать?", replyMarkup: markup);
         }
-
     }
 }
