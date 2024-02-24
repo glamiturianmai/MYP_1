6ff1920031c1fbf9fb359352e9038b465b2525c4
glamiturianmai <glamiturianmaioha@gmail.com>
2024-02-24 14:19:04 +0300

    admin work delete tg

--- MYP_MassageSalon.TG/States/AdminApplication/AdminWorkerDeleteAskState.cs
+++ MYP_MassageSalon.TG/States/AdminApplication/AdminWorkerDeleteAskState.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using MYP_MassageSalon.TG.States.ClientApplication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
 
 namespace MYP_MassageSalon.TG.States.AdminApplication
 {
-    internal class AdminWorkerDeleteAskState
+    public class AdminWorkerDeleteAskState : AbstractState
     {
+        private int _workId;
+        public AdminWorkerDeleteAskState(int id)
+        {
+            _workId = id;
+
+        }
+
+
+
+        public override AbstractState ReceiveMessage(Update update)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+
+                int workId = _workId;
+                var message = update.CallbackQuery.Data;
+
+                if (message == "ok")
+                {
+                    return new AdminWorkerDeleteState(workId);
+                }
+                else if (message == "back")
+                {
+                    return new AdminWorkerChooseState(workId);
+                }
+                else if (message == "home")
+                {
+                    return new StartState();
+                }
+
+            }
+            return this;
+        }
+
+        public override void SendMessage(long chatId)
+        {
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(
+                    new InlineKeyboardButton[][]
+                    {
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Да, хочу удадить") {CallbackData="ok"}
+
+                        },
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("назад!") {CallbackData="back"}
+                        },
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("домой!") {CallbackData="home"}
+                        }
+                    }
+                    );
+            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"А вы уверены?", replyMarkup: markup);
+        }
     }
 }
---
+++ MYP_MassageSalon.TG/States/AdminApplication/AdminWorkerDeleteState.cs
@@ -0,0 +1,61 @@
+﻿using MYP_MassageSalon.BLL.Models.InputModels;
+using MYP_MassageSalon.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace MYP_MassageSalon.TG.States.AdminApplication
+{
+    public class AdminWorkerDeleteState : AbstractState
+    {
+        private int _workId;
+        private WorkerClient _workClient;
+        private DeleteWorkerInputModel _workTG;
+
+
+        public AdminWorkerDeleteState(int workId)
+        {
+            _workId = workId;
+            _workClient = new WorkerClient();
+            _workTG = new DeleteWorkerInputModel();
+            _workTG.Id = workId;
+        }
+
+
+
+        public override AbstractState ReceiveMessage(Update update)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                return new AdminWorkerSeeState();
+            }
+            return this;
+        }
+
+        public override void SendMessage(long chatId)
+        {
+
+            _workClient.DeleteWorkerMap(_workTG); 
+
+
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(
+                   new InlineKeyboardButton[][]
+                   {
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Спасибо!") {CallbackData="SeeApps"}
+
+                        }
+                   }
+                   );
+
+            SingletoneStorage.GetStorage().Client.SendTextMessageAsync(chatId, $"Мы удалили этого мастера!", replyMarkup: markup);
+        }
+    }
+}
